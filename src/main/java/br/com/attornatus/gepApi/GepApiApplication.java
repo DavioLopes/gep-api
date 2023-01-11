fb4ef6e1ba1e022c6e53c138f38dfcac2adcf6de
Davio Lopes <davioplp@gmail.com>
2023-01-11 14:05:18 -0300

    initial commit

---
+++ src/main/java/br/com/attornatus/gepApi/GepApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.gepApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GepApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/attornatus/gepApi/GepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.gepApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
