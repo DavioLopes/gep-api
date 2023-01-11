6d45a132b31c84754b03974779d50ae588b0af8f
Davio Lopes <davioplp@gmail.com>
2023-01-11 16:11:34 -0300

    criacao do error NotFoundException

---
+++ src/main/java/br/com/attornatus/gepApi/error/NotFoundException.java
@@ -0,0 +1,11 @@
+package br.com.attornatus.gepApi.error;
+
+public class NotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
